5fd0ae4dc92d3b6b52c00eb47be69242b1a4464e
Daniel Müller <daniel.daniel@gmx.ch>
2021-11-26 03:39:04 +0100

    Basic data capture working

--- Assets/_Project/VU/Scripts/Foot.cs
+++ Assets/_Project/VU/Scripts/Foot.cs
@@ -4,10 +4,6 @@ namespace VU.Scripts
 {
     public class Foot : MonoBehaviour
     {
-        [SerializeField] Transform _modelRoot;
-
-        public Transform ModelRoot => _modelRoot;
-
         public void AttachTo(Transform rootObject)
         {
             transform.SetParent(rootObject);
