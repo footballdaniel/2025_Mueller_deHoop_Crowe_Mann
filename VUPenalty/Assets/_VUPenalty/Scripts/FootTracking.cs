6b544452e31796d66b207637965f4c8e231fcec3
Daniel Müller <daniel.daniel@gmx.ch>
2022-06-07 16:08:05 +0200

    Move to XR

---
+++ VUPenalty/Assets/_VUPenalty/Scripts/FootTracking.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class FootTracking : MonoBehaviour
+{
+    PlayerInputActions _input;
+
+    void OnEnable()
+    {
+        _input = new PlayerInputActions();
+        _input.Foot.Enable();
+    }
+
+
+    void Update()
+    {
+        transform.position = _input.Foot.Position.ReadValue<Vector3>();
+        transform.rotation = _input.Foot.Rotation.ReadValue<Quaternion>();
+    }
+
+    void OnDisable()
+    {
+        _input.Foot.Disable();
+    }
+}
